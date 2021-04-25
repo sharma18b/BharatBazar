6d56669b224dadcfc2f3b3dd85f7c5fe8c34572b
Rishabh Sharma <risharma18b@gmail.com>
2021-04-25 13:33:54 +0530

    Done till part  23

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
